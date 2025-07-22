63b253ef950d9abd2ebaabdb08d1778243755d27
soongu <naong526@naver.com>
2025-07-22 10:57:07 +0900

    Initial commit

---
+++ src/main/java/com/spring/database/SpringDb202507Application.java
@@ -0,0 +1,13 @@
+package com.spring.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDb202507Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDb202507Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/database/SpringDb202507ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb202507ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
