f1d476abd0edc57a26fc262b44ceee75eefecaf7
soongu <naong526@naver.com>
2025-07-25 10:46:38 +0900

    반려동물 관리 기능 테스트 추가: findAll, findById, save, update, delete, count 메서드 테스트 작성 및 Mapper 변경 (ORDER BY 추가).

--- src/test/java/com/spring/database/chap03/PetMapperTest.java
+++ src/test/java/com/spring/database/chap03/PetMapperTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -19,14 +21,81 @@ class PetMapperTest {
     void saveTest() {
         //given
         Pet newPet = Pet.builder()
-                .petName("꿀꿀이")
-                .petAge(8)
-                .injection(true)
+                .petName("타이거")
+                .petAge(30)
+                .injection(false)
                 .build();
         //when
         boolean save = petMapper.save(newPet);
         //then
         assertTrue(save);
+    }
+
+
+    @Test
+    @DisplayName("update test")
+    void updateTest() {
+        //given
+        Pet updatePet = Pet.builder()
+                .petName("랑이랑이")
+                .petAge(20)
+                .injection(true)
+                .id(4L)
+                .build();
+        //when
+        boolean update = petMapper.update(updatePet);
+        //then
+        assertTrue(update);
+    }
+
+
+    @Test
+    @DisplayName("delete test")
+    void deleteTest() {
+        //given
+        Long id = 2L;
+        //when
+        boolean b = petMapper.deleteById(id);
+        //then
+        assertTrue(b);
+    }
+
+
+    @Test
+    @DisplayName("findAll test")
+    void findAllTest() {
+        //given
+
+        //when
+        List<Pet> petList = petMapper.findAll();
+        //then
+        petList.forEach(System.out::println);
+        assertEquals(3, petList.size());
+    }
+
+
+    @Test
+    @DisplayName("findOne test")
+    void findOneTest() {
+        //given
+        Long id = 3L;
+        //when
+        Pet foundPet = petMapper.findById(id);
+        //then
+        System.out.println("foundPet = " + foundPet);
+        assertEquals("멍멍멍", foundPet.getPetName());
+    }
+
+
+    @Test
+    @DisplayName("count test")
+    void countTest() {
+        //given
+
+        //when
+        int count = petMapper.petCount();
+        //then
+        assertEquals(3, count);
     }
 
 
