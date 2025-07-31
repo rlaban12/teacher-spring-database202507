a95a4867aa7d6b7fb1803d5a8be343b60501ebc9
soongu <naong526@naver.com>
2025-07-31 10:51:42 +0900

    아이돌 조회 및 반환 기능 테스트 추가: 이름과 나이로 아이돌 검색 테스트 메서드 작성(searchTest), 다양한 조회 결과 반환(fetch, fetchFirst 등) 테스트 메서드 작성(fetchTest).

--- src/main/java/com/spring/database/querydsl/repository/IdolRepository.java
+++ src/main/java/com/spring/database/querydsl/repository/IdolRepository.java
@@ -4,5 +4,4 @@ import com.spring.database.querydsl.entity.Idol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IdolRepository extends JpaRepository<Idol, Long> {
-
 }
--- src/test/java/com/spring/database/querydsl/repository/QueryDslBasicTest.java
+++ src/test/java/com/spring/database/querydsl/repository/QueryDslBasicTest.java
@@ -13,6 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 import static com.spring.database.querydsl.entity.QIdol.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -159,5 +162,78 @@ class QueryDslBasicTest {
     }
 
 
+    @Test
+    @DisplayName("이름 AND 나이로 아이돌 조회하기")
+    void searchTest() {
+        //given
+        String name = "리즈";
+        int age = 20;
+
+        //when
+        Idol foundIdol = factory
+                .selectFrom(idol)
+                .where(
+                        idol.idolName.eq(name)
+                                .and(idol.age.eq(age))
+                )
+                .fetchOne();
+
+        //then
+        System.out.println("\n\nfoundIdol = " + foundIdol);
+        System.out.println("foundIdol.getGroup() = " + foundIdol.getGroup());
+
+//        idol.idolName.eq("리즈") // idolName = '리즈'
+//        idol.idolName.ne("리즈") // idolName != '리즈'
+//        idol.idolName.eq("리즈").not() // idolName != '리즈'
+//        idol.idolName.isNotNull() //이름이 is not null
+//        idol.age.in(10, 20) // age in (10,20)
+//        idol.age.notIn(10, 20) // age not in (10, 20)
+//        idol.age.between(10,30) //between 10 and 30
+//        idol.age.goe(30) // age >= 30
+//        idol.age.gt(30) // age > 30
+//        idol.age.loe(30) // age <= 30
+//        idol.age.lt(30) // age < 30
+//        idol.idolName.like("_김%")  // like _김%
+//        idol.idolName.contains("김") // like %김%
+//        idol.idolName.startsWith("김") // like 김%
+//        idol.idolName.endsWith("김") // like %김
+
+    }
+
+
+
+    @Test
+    @DisplayName("다양한 조회결과 반환하기")
+    void fetchTest() {
+
+        // fetch() : 다중 행 조회
+        List<Idol> idolList = factory
+                .selectFrom(idol)
+                .fetch();
+
+        System.out.println("\n\n============  fetch  ============");
+        idolList.forEach(System.out::println);
+
+        // fetchFirst() : 다중행 조회에서 첫번째 행을 반환
+        Idol fetchFirst = factory
+                .selectFrom(idol)
+                .where(idol.age.loe(22))
+                .fetchFirst();
+
+        System.out.println("\n\n=========== fetch first =============");
+        System.out.println("fetchFirst = " + fetchFirst);
+
+        // 단일행 조회시 NPE에 취약하기 때문에 Optional을 사용하고 싶을 때는??
+        Optional<Idol> fetchOne = Optional.ofNullable(
+                factory
+                        .selectFrom(idol)
+                        .where(idol.idolName.eq("김채원123"))
+                        .fetchOne()
+        );
+
+        Idol foundIdol = fetchOne.orElse(new Idol());
+
+
+    }
 
 }
